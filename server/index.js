81b056770c99bc0b4a6cba2901c6db0863aade84
JesseGit5151 <jrodriguez5151yahoo.com>
2023-07-05 14:11:24 -0700

    d

--- server/index.js
+++ server/index.js
@@ -24,9 +24,7 @@ app.use(passport.initialize())
 require("./config/passport")
 const PORT = process.env.PORT
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 
 // Assuming you have an express middleware to verify JWTs and add the user data to the request object
 app.use((req, res, next) => {
