a6e89825d81fd48bd6d7425847da6a2f706d3d6a
JesseGit5151 <jrodriguez5151yahoo.com>
2023-07-06 13:30:03 -0700

    a

--- client/src/components/AddPost.jsx
+++ client/src/components/AddPost.jsx
@@ -40,10 +40,7 @@ const AddPost = ({ onAddPost }) => {
       crossDomain: true,
       headers: {
         Authorization: localStorage.getItem("token"),
-        "Content-Type": "application/json",
-          Accept: "application/json",
-          "Access-Control-Allow-Origin": "*",
-          credentials: "include"
+        
       },
       body: userFormData,
     })
