a02d330b3740b776cf106effd92ce1dcaf2e323c
JesseGit5151 <jrodriguez5151yahoo.com>
2023-07-11 17:03:22 -0700

    s

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ require("./config/database")
 //Tell app what to use
 app.use("/images", express.static("images"));
 app.use("/userImages", express.static("userImages"));
-app.use(express.json())
+app.use(express.json({limit: '100mb'}))
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors({
   origin: "*",
