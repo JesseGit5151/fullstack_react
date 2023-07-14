2898ec40715a9fd787d6948c16a41434b10f6d14
JesseGit5151 <jrodriguez5151yahoo.com>
2023-07-14 13:49:42 -0700

    s

--- client/src/App.js
+++ client/src/App.js
@@ -63,17 +63,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/auth/login" element={<Login />} />
-        <Route path="/posts" element={<ProtectedRoute />}>
-    <Route
-      element={
-        <>
-          <Navbar />
-          <Searchbar onSearch={handleSearch} />
-          <Posts data={data} getData={getData} onDeletePost={handleDeletePost} />
-          <AddPost onAddPost={handleAddPost} />
-        </>
-      }
-    />
+        <Route element={<ProtectedRoute />}>
+    <Route path="/posts" element={
+      <>
+        <Navbar />
+        <Searchbar onSearch={handleSearch} />
+        <Posts data={data} getData={getData} onDeletePost={handleDeletePost} />
+        <AddPost onAddPost={handleAddPost} />
+      </>
+    } />
   </Route>
       </Routes>
     </div>
