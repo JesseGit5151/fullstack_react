f81be41faa4564437032e7792c5b57ab8e85223d
JesseGit5151 <jrodriguez5151yahoo.com>
2023-09-14 12:33:06 -0700

    updated profile component

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -6,10 +6,9 @@ import Settings from "./Settings"
 import styles from "../assets/styles/profile.module.css"
 import ProfilePostCard from './ProfilePostCard'
 const Profile = ({ onDeletePost }) => {
-  const [data, setData] = useState([])
   const [activeButton, setActiveButton] = useState('profile');
   const [mainContent, setMainContent] = useState(<ProfilePostCard/>);
-  const [isLoading, setIsLoading] = useState(true)
+  
   //call fetch to retreive personal posts
   //fetch data from /posts route
   const handleButtonClick = (buttonId, content) => {
@@ -17,40 +16,40 @@ const Profile = ({ onDeletePost }) => {
     setMainContent(content);
   };
 
-  async function getData(query = "") {
-    try {
-      const response = await fetch(
-        `https://yourfavorites-api.onrender.com/posts?query=${query}`,
-        {
-          headers: {
-            Authorization: localStorage.getItem("token"),
-            "Cache-Control": "no-store",
-          },
-        }
-      )
-      const data = await response.json()
-      console.log(data)
-      setData(data)
-      setIsLoading(false)
-    } catch (error) {
-      console.error("An error occurred:", error)
-    }
-  }
-  useEffect(() => {
+//   async function getData(query = "") {
+//     try {
+//       const response = await fetch(
+//         `https://yourfavorites-api.onrender.com/posts?query=${query}`,
+//         {
+//           headers: {
+//             Authorization: localStorage.getItem("token"),
+//             "Cache-Control": "no-store",
+//           },
+//         }
+//       )
+//       const data = await response.json()
+//       console.log(data)
+//       setData(data)
+//       setIsLoading(false)
+//     } catch (error) {
+//       console.error("An error occurred:", error)
+//     }
+//   }
+//   useEffect(() => {
 
-    getData()
-}, [])
+//     getData()
+// }, [])
 
-const handleDeletePost = (postId) => {
+// const handleDeletePost = (postId) => {
     
-  onDeletePost(postId)
-}
+//   onDeletePost(postId)
+// }
   return (
     <>
       <Navbar/>
       {/* create a div for the tags: profile - saved - settings */}
       <ul className={styles.subnav}>
-        <li id='profile' onClick={() => handleButtonClick('profile', <ProfilePostCard item={data}/>)}>Profile</li>
+        <li id='profile' onClick={() => handleButtonClick('profile', <ProfilePostCard />)}>Profile</li>
         <li id='saves' onClick={() => handleButtonClick('saves', <Saves/>)}>Saves</li>
         <li id='settings' onClick={() => handleButtonClick('settings', <Settings/>)}>Settings</li>
         </ul>
--- client/src/components/ProfilePostCard.jsx
+++ client/src/components/ProfilePostCard.jsx
@@ -1,20 +1,49 @@
 import React from 'react'
 import styles from "../assets/styles/post.module.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
-const ProfilePostCard = ({ item, onDeletePost }) => {
+const ProfilePostCard = ({ onDeletePost }) => {
+    const [data, setData] = useState([])
     const [isHovering, setIsHovering] = useState(false)
-    // const timestamp = item.createdAt
-  // Convert the timestamp to a Date object
-// const dateObj = new Date(timestamp);
+    const [isLoading, setIsLoading] = useState(true)
+    const timestamp = item.createdAt
+  //Convert the timestamp to a Date object
+const dateObj = new Date(timestamp);
 
-// Format the date to day/month/year
-// const formattedDate = dateObj.toLocaleDateString("en-US", {
-//   day: "2-digit",
-//   month: "2-digit",
-//   year: "numeric"
-// });
+//Format the date to day/month/year
+const formattedDate = dateObj.toLocaleDateString("en-US", {
+  day: "2-digit",
+  month: "2-digit",
+  year: "numeric"
+});
 
+async function getData(query = "") {
+    try {
+      const response = await fetch(
+        `https://yourfavorites-api.onrender.com/posts?query=${query}`,
+        {
+          headers: {
+            Authorization: localStorage.getItem("token"),
+            "Cache-Control": "no-store",
+          },
+        }
+      )
+      const data = await response.json()
+      console.log(data)
+      setData(data)
+      setIsLoading(false)
+    } catch (error) {
+      console.error("An error occurred:", error)
+    }
+  }
+  useEffect(() => {
+
+    getData()
+}, [])
+const handleDeletePost = (postId) => {
+    
+    onDeletePost(postId)
+  }
 const handleMouseEnter = (e) => {
     setIsHovering(true)
   }
@@ -23,19 +52,31 @@ const handleMouseEnter = (e) => {
     setIsHovering(false)
   }
   return (
-    <div className={styles.container}>
-      {/* add height: 300px & border-radius: inherit; */}
-      <div className={`${isHovering ? styles.hoverStyle : styles.h}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      {/* <img className={styles.image} src={`https://yourfavorites-api.onrender.com/${item.image}`} />
-      <a href='#' className={`${isHovering ? styles.link : styles.hidden}`}>{item.description}</a>
-      </div>
-      <div className={styles.imgInfo}>
+    
+      <div>{isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {data.length > 0 ? (
+            <div className={styles.container}>
+             
+              {data.map((item, index) => {
+             return <><div className={`${isHovering ? styles.hoverStyle : styles.h}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img className={styles.image} src={`https://yourfavorites-api.onrender.com/${item.image}`} />
+             <a href='#' className={`${isHovering ? styles.link : styles.hidden}`}>{item.description}</a></div>
+             <div className={styles.imgInfo}>
       <h4 className={styles.title}>
       {item.title}
-      </h4> */}
-      {/* <span className={styles.date}>{formattedDate}</span> */}
+      </h4>
+      <span className={styles.date}>{formattedDate}</span>
       </div>
-    </div>
+             </>
+           })}
+            </div>
+          ) : (
+            <div>No content saved yet.</div>
+          )}
+        </div>
+      )}</div>
   )
 }
 
