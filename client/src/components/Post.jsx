d3e002929ed452253e6af7def8ae3e9071f72628
JesseGit5151 <jrodriguez5151yahoo.com>
2023-09-26 12:49:27 -0700

    added line under active button

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,46 +1,55 @@
-import React from 'react'
+import React from "react"
 import styles from "../assets/styles/post.module.css"
 import { useState } from "react"
-import { FaRegTrashAlt } from 'react-icons/fa';
+import { FaRegTrashAlt } from "react-icons/fa"
 const Post = ({ item, onDeletePost }) => {
   const [isHovering, setIsHovering] = useState(false)
   const timestamp = item.createdAt
   // Convert the timestamp to a Date object
-const dateObj = new Date(timestamp);
+  const dateObj = new Date(timestamp)
 
-// Format the date to day/month/year
-const formattedDate = dateObj.toLocaleDateString("en-US", {
-  day: "2-digit",
-  month: "2-digit",
-  year: "numeric"
-});
+  // Format the date to day/month/year
+  const formattedDate = dateObj.toLocaleDateString("en-US", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  })
 
-const handleMouseEnter = (e) => {
-  setIsHovering(true)
-}
+  const handleMouseEnter = (e) => {
+    setIsHovering(true)
+  }
 
-const handleMouseLeave = (e) => {
-  setIsHovering(false)
-}
+  const handleMouseLeave = (e) => {
+    setIsHovering(false)
+  }
   // const handleClick = async () => {
   //   onDeletePost(item._id);
   // }
 
-
   return (
     <div className={styles.container}>
       {/* add height: 300px & border-radius: inherit; */}
-      <div className={`${isHovering ? styles.hoverStyle : styles.h}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <img className={styles.image} src={`https://yourfavorites-api.onrender.com/${item.image}`} />
-      <a href='#' className={`${isHovering ? styles.link : styles.hidden}`}>{item.description}</a>
+      <div
+        className={`${isHovering ? styles.hoverStyle : styles.h}`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <img
+          className={styles.image}
+          src={`https://yourfavorites-api.onrender.com/${item.image}`}
+        />
+        <a href="#" className={`${isHovering ? styles.link : styles.hidden}`}>
+          {item.description}
+        </a>
       </div>
       <div className={styles.imgInfo}>
-      <img className={styles.avatar} src={`https://yourfavorites-api.onrender.com/${item.author.avatar}`} />
-      <h3 className={styles.author}>{item.author.username}</h3>
-      <h4 className={styles.title}>
-      {item.title}
-      </h4>
-      <span className={styles.date}>{formattedDate}</span>
+        <img
+          className={styles.avatar}
+          src={`https://yourfavorites-api.onrender.com/${item.author.avatar}`}
+        />
+        <h3 className={styles.author}>{item.author.username}</h3>
+        <h4 className={styles.title}>{item.title}</h4>
+        <span className={styles.date}>{formattedDate}</span>
       </div>
     </div>
   )
