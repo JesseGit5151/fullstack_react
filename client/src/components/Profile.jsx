0c190781d61c1affaeb775c43c4acae87734b82d
JesseGit5151 <jrodriguez5151yahoo.com>
2023-09-29 14:17:41 -0700

    a

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -40,7 +40,7 @@ const Profile = ({ onDeletePost }) => {
 //     getData()
 // }, [])
 
-const onDeletePost = (postId) => {
+const handleDelete = (postId) => {
     
   onDeletePost(postId)
 }
@@ -49,7 +49,7 @@ const onDeletePost = (postId) => {
       <Navbar/>
       {/* create a div for the tags: profile - saved - settings */}
       <ul className={styles.subnav}>
-        <li className={activeButton === 'profile' ? styles.underline : ''} id='profile' onClick={() => handleButtonClick('profile', <ProfilePostCard onDeletePost={onDeletePost}/>)}>Profile</li>
+        <li className={activeButton === 'profile' ? styles.underline : ''} id='profile' onClick={() => handleButtonClick('profile', <ProfilePostCard onDeletePost={handleDelete}/>)}>Profile</li>
         <li className={activeButton === 'saves' ? styles.underline : ''} id='saves' onClick={() => handleButtonClick('saves', <Saves/>)}>Saves</li>
         <li className={activeButton === 'settings' ? styles.underline : ''} id='settings' onClick={() => handleButtonClick('settings', <Settings/>)}>Settings</li>
         </ul>
