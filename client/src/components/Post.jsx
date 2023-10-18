f29669ee25c218a6f403479486123043eb6d9e57
JesseGit5151 <jrodriguez5151yahoo.com>
2023-10-18 15:37:41 -0700

    updated heart icon class

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -49,7 +49,7 @@ const Post = ({ item }) => {
         />
         <h3 className={styles.author}>{item.author.username}</h3>
         <h4 className={styles.title}>{item.title}</h4>
-        <i className="fa fa-heart-o"></i>
+        <i className="far fa-heart"></i>
         <span className={styles.date}>{formattedDate}</span>
       </div>
     </div>
