e03f1cfeb9216eb5c6c7820224f5f008241d4156
JesseGit5151 <jrodriguez5151yahoo.com>
2023-12-14 15:01:42 -0800

    asa

--- client/src/components/Saves.jsx
+++ client/src/components/Saves.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 
 const Saves = () => {
-  const [data, setData] = useState()
+  const [data, setData] = useState([])
   //Fetch saves
   async function getLikes() {
     try {
